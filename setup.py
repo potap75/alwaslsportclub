38905d1552ba2d061ab21739bce198bfa77b8275
potap75 <rummaan@me.com>
2023-05-13 03:45:07 +0400

    First commit with alembic

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='alwaslsportclub',
+    version='',
+    packages=['app'],
+    url='',
+    license='',
+    author='Roman Consulting FZ-LLC',
+    author_email='info@romanconsulting.io',
+    description=''
+)
